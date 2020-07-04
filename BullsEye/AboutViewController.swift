318102852dec544f12808c93e138a9fac8c3a150
myko <awesomeme155@gmail.com>
2020-07-04 19:58:34 +0300

    add web view to AboutViewController

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let url = Bundle.main.url(forResource: "Bullseye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
     
 
